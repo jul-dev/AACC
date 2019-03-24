f912eb0db413d58a49ebbe9821324f2790583341
julia <jkovrigin@gmail.com>
2019-03-24 17:13:29 -0400

    updated file with if statements

--- CTP 115/Homework/HW3/src/InsuranceApp.java
+++ CTP 115/Homework/HW3/src/InsuranceApp.java
@@ -2,12 +2,12 @@ public class InsuranceApp {
     public static void main(String[] args) {
     //Create and initialize a PolicyHolder object using the default constructor, naming it newPolicyHolder.
         PolicyHolder newPolicyHolder= new PolicyHolder();
-        newPolicyHolder.setCustomerAge(13);
+        newPolicyHolder.setCustomerAge(45);
         newPolicyHolder.setPolicyNumber(1234);
         newPolicyHolder.setNumberOfAccidents(1);
 
         PolicyHolder ph2= new PolicyHolder(142, 2,0);
-        PolicyHolder ph3= new PolicyHolder(345, 34,3);
+        PolicyHolder ph3= new PolicyHolder(345, 60,0);
 
         checkAccident(newPolicyHolder);
         checkAccident(ph2);
@@ -15,8 +15,10 @@ public class InsuranceApp {
     }
 
     public static void checkAccident(PolicyHolder ph){
-        if ()
-        ph.displayPolicyHolder();
+        if ((ph.getCustomerAge()>35)&& (ph.getNumberOfAccidents()<=1)){
+            ph.displayPolicyHolder();
+
+        }
 
     }
 }
